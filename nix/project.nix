67090f1483940c9d7140a7e5a41fd24d7182152d
Jann Müller <j-mueller@users.noreply.github.com>
2024-12-17 10:23:17 +0100

    Add catalyst-onchain-libs to sha256map; ignore pre-commit-yaml

--- nix/project.nix
+++ nix/project.nix
@@ -3,6 +3,7 @@
 let
   sha256map = {
     "https://github.com/colll78/plutarch-plutus"."b2379767c7f1c70acf28206bf922f128adc02f28" = "sha256-mhuW2CHxnc6FDWuMcjW/51PKuPOdYc4yxz+W5RmlQew=";
+    "https://github.com/input-output-hk/catalyst-onchain-libs"."25996ca26f1b33ffef611bc81423aae5841e297f" = "sha256-NUh+l97+eO27Ppd8Bx0yMl0E5EV+p7+7GuFun1B8gRc=";
   };
 
   modules = [{ }];
