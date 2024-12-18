c4ce219845fc3549ae38e2d5897cd27aeeb34615
Jann Müller <j-mueller@users.noreply.github.com>
2024-12-18 09:17:21 +0100

    Update dependency

--- nix/project.nix
+++ nix/project.nix
@@ -3,7 +3,7 @@
 let
   sha256map = {
     "https://github.com/colll78/plutarch-plutus"."b2379767c7f1c70acf28206bf922f128adc02f28" = "sha256-mhuW2CHxnc6FDWuMcjW/51PKuPOdYc4yxz+W5RmlQew=";
-    "https://github.com/input-output-hk/catalyst-onchain-libs"."25996ca26f1b33ffef611bc81423aae5841e297f" = "sha256-NUh+l97+eO27Ppd8Bx0yMl0E5EV+p7+7GuFun1B8gRc=";
+    "https://github.com/input-output-hk/catalyst-onchain-libs"."650a3435f8efbd4bf36e58768fac266ba5beede4" = "sha256-NUh+l97+eO27Ppd8Bx0yMl0E5EV+p7+7GuFun1B8gRc=";
   };
 
   modules = [{ }];
