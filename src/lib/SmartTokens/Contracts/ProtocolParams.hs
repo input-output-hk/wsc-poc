4d5a88714b5315e28493acff084918f0c4ba7760
Philip DiSarro <philipdisarro@gmail.com>
2024-12-20 17:14:38 -0800

    fix bugs and improve tracing

--- src/lib/SmartTokens/Contracts/ProtocolParams.hs
+++ src/lib/SmartTokens/Contracts/ProtocolParams.hs
@@ -1,37 +1,23 @@
-{-# LANGUAGE QualifiedDo #-}
-{-# LANGUAGE OverloadedRecordDot  #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE QualifiedDo         #-}
 module SmartTokens.Contracts.ProtocolParams (
   mkProtocolParametersMinting,
   alwaysFailScript,
   mkPermissionedMinting,
 ) where
 
-import Plutarch.LedgerApi.V3 ( PTxOutRef, PScriptContext, PPubKeyHash )
+import Plutarch.Builtin (PAsData, PData, pconstantData, pfromData, pfstBuiltin,
+                         psndBuiltin)
+import Plutarch.Core.Utils (phasUTxO, pheadSingleton, pletFieldsMinting,
+                            ptryLookupValue, ptxSignedByPkh,
+                            pvalidateConditions)
+import Plutarch.LedgerApi.V3 (PPubKeyHash, PScriptContext, PTxOutRef)
 import Plutarch.Monadic qualified as P
-import Plutarch.Prelude
-    ( PAsData,
-      PData,
-      PEq((#==)),
-      type (:-->),
-      (#),
-      plet,
-      perror,
-      pconstantData,
-      ClosedTerm,
-      plam,
-      pfromData,
-      pfstBuiltin,
-      psndBuiltin,
-      pletFields,
-      PUnit, pfield )
-import Plutarch.Core.Utils
-    ( pheadSingleton,
-      ptryLookupValue,
-      phasUTxO,
-      pvalidateConditions,
-      pletFieldsMinting, ptxSignedByPkh )
-import SmartTokens.Types.Constants ( pprotocolParamsTokenData ) 
+import Plutarch.Prelude (ClosedTerm, PEq ((#==)), PUnit, perror, pfield, plam,
+                         plet, pletFields, type (:-->), (#))
+import Plutarch.Trace (ptraceInfoIfFalse)
+import SmartTokens.Types.Constants (pprotocolParamsTokenData)
 
 -- | Protocol Parameters minting policy
 -- This validator allows minting of a single token with a single token name.
@@ -40,30 +26,30 @@ mkProtocolParametersMinting = plam $ \oref ctx -> P.do
   ctxF <- pletFields @'["txInfo", "scriptInfo"] ctx
   infoF <- pletFields @'["inputs", "mint"] ctxF.txInfo
   scriptInfoF <- pletFieldsMinting ctxF.scriptInfo
-  let ownCS = scriptInfoF._0 
+  let ownCS = scriptInfoF._0
   mintedValue <- plet $ pfromData infoF.mint
   let ownTkPairs = ptryLookupValue # ownCS # mintedValue
-  -- Enforce that only a single token name is minted for this policy 
+  -- Enforce that only a single token name is minted for this policy
   ownTkPair <- plet (pheadSingleton # ownTkPairs)
   ownTokenName <- plet (pfstBuiltin # ownTkPair)
   ownNumMinted <- plet (psndBuiltin # ownTkPair)
   pvalidateConditions
-    [ ownTokenName #== pprotocolParamsTokenData
-    , ownNumMinted #== pconstantData 1
-    , phasUTxO # oref # infoF.inputs
+    [ ptraceInfoIfFalse "token name mismatch" $ ownTokenName #== pprotocolParamsTokenData
+    , ptraceInfoIfFalse "incorrect minted amount" $ ownNumMinted #== pconstantData 1
+    , ptraceInfoIfFalse "init UTxO not present" $ phasUTxO # oref # pfromData infoF.inputs
     ]
 
 -- | Permissioned Minting Policy
 -- This minting policy checks for a given permissioned credential in the signatories of the transaction.
 -- It allows minting of any number of tokens with any token name so long as the credential authorizes the transaction.
 mkPermissionedMinting :: ClosedTerm (PAsData PPubKeyHash :--> PScriptContext :--> PUnit)
-mkPermissionedMinting = plam $ \permissionedCred ctx -> 
+mkPermissionedMinting = plam $ \permissionedCred ctx ->
   pvalidateConditions
     [ ptxSignedByPkh # permissionedCred # (pfield @"signatories" # (pfield @"txInfo" # ctx))
     ]
 
 -- | A nonced always fails script
 -- The parameter is used to modify the script hash.
--- This is where the protocol parameters UTxO should reside. 
+-- This is where the protocol parameters UTxO should reside.
 alwaysFailScript :: ClosedTerm (PData :--> PScriptContext :--> PUnit)
-alwaysFailScript = plam $ \_ _ctx -> perror 
+alwaysFailScript = plam $ \_ _ctx -> perror
--- src/lib/SmartTokens/LinkedList/SpendDirectory.hs
+++ src/lib/SmartTokens/LinkedList/SpendDirectory.hs
@@ -1,44 +1,29 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE AllowAmbiguousTypes  #-}
+{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE OverloadedRecordDot  #-}
-{-# LANGUAGE QualifiedDo #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QualifiedDo          #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module SmartTokens.LinkedList.SpendDirectory (pmkDirectorySpending, pmkDirectorySpendingYielding, pmkDirectoryGlobalLogic) where 
+module SmartTokens.LinkedList.SpendDirectory (pmkDirectorySpending, pmkDirectorySpendingYielding, pmkDirectoryGlobalLogic) where
 
-import Plutarch.LedgerApi.V3
-    ( PCredential,
-      PCurrencySymbol,
-      POutputDatum(POutputDatum),
-      PScriptContext ) 
-import Plutarch.Monadic qualified as P
-import Plutarch.Prelude
-    ( (#),
-      (#$),
-      perror,
-      pto,
-      pmatch,
-      type (:-->),
-      ClosedTerm,
-      plam,
-      pconstant,
-      pfromData,
-      pfield,
-      pletFields,
-      PAsData,
-      PBuiltinList,
-      PMaybe(PNothing, PJust),
-      PUnit )
-import Plutarch.Unsafe (punsafeCoerce)
-import Plutarch.Core.Utils (pvalidateConditions, phasDataCS, pmustFind)
+import Plutarch.Core.Utils (phasDataCS, pmustFind, pvalidateConditions)
 import Plutarch.LedgerApi.AssocMap qualified as AssocMap
+import Plutarch.LedgerApi.V3 (PCredential, PCurrencySymbol,
+                              POutputDatum (POutputDatum), PScriptContext)
+import Plutarch.Monadic qualified as P
+import Plutarch.Prelude (ClosedTerm, PAsData, PBuiltinList,
+                         PMaybe (PJust, PNothing), PUnit, pconstant, perror,
+                         pfield, pfromData, plam, pletFields, pmatch, pto,
+                         type (:-->), (#$), (#))
+import Plutarch.Trace
+import Plutarch.Unsafe (punsafeCoerce)
 import SmartTokens.Types.ProtocolParams (PProgrammableLogicGlobalParams)
 
 pmkDirectoryGlobalLogic :: ClosedTerm (PAsData PCurrencySymbol :--> PScriptContext :--> PUnit)
-pmkDirectoryGlobalLogic = plam $ \protocolParamsCS ctx -> P.do 
+pmkDirectoryGlobalLogic = plam $ \protocolParamsCS ctx -> P.do
   ctxF <- pletFields @'["txInfo", "scriptInfo"] ctx
   infoF <- pletFields @'["referenceInputs", "mint"] ctxF.txInfo
   let paramUTxO =
@@ -55,17 +40,17 @@ pmkDirectoryGlobalLogic = plam $ \protocolParamsCS ctx -> P.do
 
 pmkDirectorySpendingYielding ::
   ClosedTerm (PAsData PCredential :--> PScriptContext :--> PUnit)
-pmkDirectorySpendingYielding = plam $ \globalCred ctx -> P.do 
+pmkDirectorySpendingYielding = plam $ \globalCred ctx -> P.do
   ctxF <- pletFields @'["txInfo"] ctx
-  let stakeCerts = pfield @"wdrl" # ctxF.txInfo 
+  let stakeCerts = pfield @"wdrl" # ctxF.txInfo
       stakeScript = pfromData globalCred
-  pmatch (AssocMap.plookup # stakeScript # stakeCerts) $ \case 
-    PJust _ -> (pconstant ()) 
-    PNothing -> perror 
+  pmatch (AssocMap.plookup # stakeScript # stakeCerts) $ \case
+    PJust _ -> (pconstant ())
+    PNothing -> perror
 
 pmkDirectorySpending ::
   ClosedTerm (PAsData PCurrencySymbol :--> PScriptContext :--> PUnit)
-pmkDirectorySpending = plam $ \protocolParamsCS ctx -> P.do 
+pmkDirectorySpending = plam $ \protocolParamsCS ctx -> P.do
   ctxF <- pletFields @'["txInfo", "scriptInfo"] ctx
   infoF <- pletFields @'["referenceInputs", "mint"] ctxF.txInfo
   let paramUTxO =
@@ -73,9 +58,9 @@ pmkDirectorySpending = plam $ \protocolParamsCS ctx -> P.do
           pmustFind @PBuiltinList
             # plam (\txIn ->
                     let resolvedIn = pfield @"resolved" # txIn
-                    in phasDataCS # protocolParamsCS # (pfield @"value" # resolvedIn)
+                    in phasDataCS # protocolParamsCS # pfromData (pfield @"value" # resolvedIn)
                   )
-            # infoF.referenceInputs 
+            # pfromData infoF.referenceInputs
   POutputDatum ((pfield @"outputDatum" #) -> paramDat') <- pmatch $ pfield @"datum" # paramUTxO
   protocolParamsF <- pletFields @'["directoryNodeCS"] (pfromData $ punsafeCoerce @_ @_ @(PAsData PProgrammableLogicGlobalParams) (pto paramDat'))
-  pvalidateConditions [phasDataCS # protocolParamsF.directoryNodeCS # pfromData infoF.mint]
+  pvalidateConditions [ptraceInfoIfFalse "cannot find directory CS" $ phasDataCS # protocolParamsF.directoryNodeCS # pfromData infoF.mint]
