0bdc09d17951c1123885120a3b9d3086941693ce
Christian Hoener zu Siederdissen <christian.hoener@iohk.io>
2024-12-26 06:30:29 -0300

    adds building oci containers (#28)
    * Uses the haskell.nix container building infrastructure
    
    * Is convoluted, containers are not directly built, only the manifest,
      or here a copy-to system
    
    * Resulting tar is about 32 MByte, not tested yet

---
+++ nix/containers.nix
@@ -0,0 +1,18 @@
+{ repoRoot, inputs, pkgs, lib, system }:
+{
+
+  # Builds a docker container for the cabal executable given as input. First we
+  # build the container json itself. Note the explicit architecture.
+  #
+  # $ nix build .#containers.x86_64-linux.wst
+  #
+  wst = lib.iogx.mkContainerFromCabalExe {
+    exe = inputs.self.packages.wst-poc;
+    name = "wst-poc";
+    description = "WST Proof of Concept";
+    packages = [ ];
+    sourceUrl = "https://github.com/input-output-hk/wsc-poc";
+  };
+
+}
+
