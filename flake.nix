3c51eb37eece00efab912d0dc98ea91338760fcf
Jann Müller <j-mueller@users.noreply.github.com>
2025-04-20 12:18:09 +0200

    Add nix cache

--- flake.nix
+++ flake.nix
@@ -56,10 +56,12 @@
     extra-substituters = [
       "https://cache.iog.io"
       "https://cache.zw3rk.com"
+      "https://cache.ml42.de"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+      "cache.ml42.de:RKmSRP9TOc87nh9FZCM/b/pMIE3kBLEeIe71ReCBwRM="
     ];
     allow-import-from-derivation = true;
   };
